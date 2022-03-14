ced9527da0227c0fd6faf2ff387b56189a1924b8
gzonelee <gzonelee@gmail.com>
2022-03-15 00:26:29 +0900

    Initial Commit

---
+++ GBufferRender macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  GBufferRender macOS
+//
+//  Created by gzonelee on 2022/03/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
